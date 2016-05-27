16f8eb8474d274b7f1c9232da0c9b9074f7cce99
Alex Kononenko <avkcorporation@users.noreply.github.com>
2016-05-27 21:16:01 +0300

    Update Filter.java
    Fix major

--- src/main/java/ua/avk/rest/service/util/Filter.java
+++ src/main/java/ua/avk/rest/service/util/Filter.java
@@ -1,26 +1,22 @@
 package ua.avk.rest.service.util;
 
-import ua.avk.rest.entity.Contact;
-
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * @author Alex Kononenko
  * @version 1.0.0
- *
- * Filter class
+ *          <p/>
+ *          Filter class
  */
 public class Filter {
     private Pattern pattern;
     private Matcher matcher;
 
     /**
-     *
      * @param regExp string
      */
-    public Filter(String regExp){
+    public Filter(String regExp) {
         pattern = Pattern.compile(regExp);
     }
 
@@ -30,13 +26,12 @@ public class Filter {
      * @param contact string
      * @return true or false
      */
-    public boolean getFilter(String contact){
+    public boolean getFilter(String contact) {
         matcher = pattern.matcher(contact);
-        if (matcher.matches()){
+        if (matcher.matches()) {
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
 

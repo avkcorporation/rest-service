b5e7adf21d0b86adfc4ec7d3adcd8aeef2b2a112
Alex Kononenko <avkcorporation@users.noreply.github.com>
2016-05-27 21:16:42 +0300

    Update ContactPage.java
    Fix major

--- src/main/java/ua/avk/rest/web/ContactPage.java
+++ src/main/java/ua/avk/rest/web/ContactPage.java
@@ -7,79 +7,80 @@ import ua.avk.rest.web.exceptions.Error;
 import ua.avk.rest.web.utils.ResponseCreator;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.*;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.Response;
 
 /**
  * @author Alex Kononenko
  * @version 1.0.0
- *
- * The rest service for contacts
+ *          <p/>
+ *          The rest service for contacts
  */
-@Path(value="/contacts")
-public class ContactPage implements IContactPage{
-
-	private JSONObject responseContactDetailsJson;
+@Path(value = "/contacts")
+public class ContactPage implements IContactPage {
+    public static final int RESPONSE_ERROR = 500;
 
     IContactSrv contactSrv;
+    private JSONObject responseContactDetailsJson;
 
-	/**
-	 * For unit tests
-	 *
-	 * @param contactSrv Contact service class
-	 */
-	public void setContactSrv(IContactSrv contactSrv) {
-		this.contactSrv = contactSrv;
-	}
+    /**
+     * Default constructor
+     */
+    public ContactPage() {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                new String[]{"classpath:/spring/applicationContext.xml"}, true);
+        contactSrv = (IContactSrv) context.getBean("contactSrv");
+    }
 
-	/**
-	 * For unit tests
-	 *
-	 * @param responseContactDetailsJson object
-	 */
-	public void setResponseContactDetailsJson(JSONObject responseContactDetailsJson) {
-		this.responseContactDetailsJson = responseContactDetailsJson;
-	}
+    /**
+     * For unit tests
+     *
+     * @param contactSrv Contact service class
+     */
+    public void setContactSrv(IContactSrv contactSrv) {
+        this.contactSrv = contactSrv;
+    }
 
-	/**
-	 * Default constructor
-	 */
-	public ContactPage() {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				new String[]{"classpath:/spring/applicationContext.xml"}, true);
-		contactSrv = (IContactSrv) context.getBean("contactSrv");
-	}
+    /**
+     * For unit tests
+     *
+     * @param responseContactDetailsJson object
+     */
+    public void setResponseContactDetailsJson(JSONObject responseContactDetailsJson) {
+        this.responseContactDetailsJson = responseContactDetailsJson;
+    }
 
-	/**
-   	 * Get data by regular expression from database
-	 *
-	 * @param nameFilter string regular expression
-   	 * @return response obj
-   	 */
-	@GET
-	@Path(value="/")
-   	@Produces("application/json")
-	public Response getNameFilter(@DefaultValue("^")@QueryParam("nameFilter") String nameFilter,
-									@DefaultValue("1")@QueryParam("id") long startId,
-									@DefaultValue("100")@QueryParam("count") long count) {
+    /**
+     * Get data by regular expression from database
+     *
+     * @param nameFilter string regular expression
+     * @return response obj
+     */
+    @GET
+    @Path(value = "/")
+    @Produces("application/json")
+    public Response getNameFilter(@DefaultValue("^") @QueryParam("nameFilter") String nameFilter,
+                                  @DefaultValue("1") @QueryParam("id") long startId,
+                                  @DefaultValue("100") @QueryParam("count") long count) {
 
-		JSONObject responseContactDetailsJson2 = new JSONObject();
-		setResponseContactDetailsJson(contactSrv.getJsonContactsByFilter(nameFilter, startId, count));
+        JSONObject responseContactDetailsJson2 = new JSONObject();
+        setResponseContactDetailsJson(contactSrv.getJsonContactsByFilter(nameFilter, startId, count));
 
-		if (responseContactDetailsJson != null) {
-			responseContactDetailsJson2.put("contacts", responseContactDetailsJson);
+        if (responseContactDetailsJson != null) {
+            responseContactDetailsJson2.put("contacts", responseContactDetailsJson);
 
-			GenericEntity<JSONObject> entity = new GenericEntity<JSONObject>(
-					responseContactDetailsJson2) {
-			};
+            GenericEntity<JSONObject> entity = new GenericEntity<JSONObject>(
+                    responseContactDetailsJson2) {
+            };
 
-			return ResponseCreator.success(entity);
+            return ResponseCreator.success(entity);
 
-		} else {
-			return ResponseCreator.error(500,
-					Error.SERVER_ERROR.getCode(),
-					Error.SERVER_ERROR.getDescription());
-		}
+        } else {
+            return ResponseCreator.error(RESPONSE_ERROR,
+                    Error.SERVER_ERROR.getCode(),
+                    Error.SERVER_ERROR.getDescription());
+        }
 
-	}
+    }
 
 }

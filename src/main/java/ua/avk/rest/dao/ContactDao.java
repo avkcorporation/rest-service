7cbad22e00b3798e3fd25266b2fc95ebe35e316f
Alex Kononenko <avkcorporation@users.noreply.github.com>
2016-07-18 17:04:08 +0300

    include library

--- src/main/java/ua/avk/rest/dao/ContactDao.java
+++ src/main/java/ua/avk/rest/dao/ContactDao.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ua.avk.rest.entity.Contact;
+import javax.annotation.Resource;
 
 import java.util.List;
 
